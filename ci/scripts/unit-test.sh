1667640a7c7b102979e0d0e13b78322d353cc4de
Mike Oswell <moswell@cisco.com>
2017-11-05 20:51:45 -0800

    Testing concourse

---
+++ ci/scripts/unit-test.sh
@@ -0,0 +1,23 @@
+set -e -x
+
+# Setup the gopath based on current directory.
+export GOPATH=$PWD
+
+# Now we must move our code from the current directory ./hello-go to $GOPATH/src/github.com/JeffDeCola/hello-go
+mkdir -p src/github.com/oswemo
+cp -R ./urlCheck src/github.com/oswemo/.
+
+# All set and everything is in the right place for go
+echo "Gopath is: " $GOPATH
+echo "pwd is: " $PWD
+cd src/github.com/oswemo/urlCheck
+ls -lat
+
+# RUN unit_tests and it shows the percentage coverage
+# print to stdout and file using tee
+go test -cover ./... | tee test_coverage.txt
+# add some whitespace to the begining of each line
+sed -i -e 's/^/     /' test_coverage.txt
+
+# Move to coverage-results directory.
+mv test_coverage.txt $GOPATH/coverage-results/.
