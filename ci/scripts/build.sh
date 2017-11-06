b3ba071f98d2a06b8a1c1dc1a4a782d9b665f204
Mike Oswell <moswell@cisco.com>
2017-11-06 08:49:00 -0800

    More stuff

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -16,14 +16,5 @@ echo "pwd is: " $PWD
 cd src/github.com/oswemo/UrlCheck
 ls -lat
 
-make deps
+make deps build
 
-# RUN unit_tests and it shows the percentage coverage
-# print to stdout and file using tee
-make test | tee test_coverage.txt
-
-# add some whitespace to the begining of each line
-sed -i -e 's/^/     /' test_coverage.txt
-
-# Move to coverage-results directory.
-mv test_coverage.txt $GOPATH/coverage-results/.
